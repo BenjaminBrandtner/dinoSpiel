72836863dd15a5fa8eaf4a5a810dfe1fac789f76
Benny <37274870+BenjaminBrandtner@users.noreply.github.com>
2018-06-13 08:46:29 +0200

    Unvollständig: SpringenderDino, Texturen geändert, abgespeichert, Unvollstädnig: Texture einlesen

--- benny/springenderDino.c
+++ benny/springenderDino.c
@@ -2,18 +2,24 @@
  * dino der springt auf KEY_UP */
 
 #include <stdio.h>
+#include <ncurses.h>
+
+void zeichneDino(int *y);
 
 int main (void)
 {
 	int eingabe;
-	int y; dy;
+	int y=15; 
+	int dy=0;
 	int schritt;
 	int sprungstart;
-	int geschwindigkeit=60; //ms verzögerung zwischen schritten
+	int geschwindigkeit=20; //ms verzögerung zwischen schritten
 
 	initscr();
 	noecho();
 	cbreak();
+	nodelay(stdscr, TRUE);
+	keypad(stdscr, TRUE);
 
 	mvprintw(15,0,"_______________________________________________");
 
@@ -21,11 +27,31 @@ int main (void)
 	while(1)
 	{
 		mvprintw(0,0,"Step: %i", schritt++);
+		if((eingabe=getch())==KEY_UP)
+		{
+			springe(
+		}
+
+		zeichneDino(&y);
+
+		refresh();
 
 
+		usleep(geschwindigkeit*10000);
+	}
 
 
 
 	endwin();
 	return 0;
 }
+
+void zeichneDino(int *y)
+{
+	mvaddch(*y,3,' ');
+	mvaddch((*y)+1,3,' ');
+
+	mvaddch(*y,3,'#');
+	mvaddch((*y)-1,3,'#');
+}
+
---
+++ philipp/texturenEinlesen.h
@@ -0,0 +1,4 @@
+/*Autor: Philipp Hägerich 
+List die passenden texturen ein und speichert sie in einen Struct*/
+
+void
