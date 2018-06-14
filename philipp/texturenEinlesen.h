05920f7f916949fe00533c31dd1337c1290c3693
Benny <37274870+BenjaminBrandtner@users.noreply.github.com>
2018-06-14 15:44:15 +0200

    texture einlesen fast fertig

--- philipp/texturenEinlesen.h
+++ philipp/texturenEinlesen.h
@@ -1,4 +1,24 @@
 /*Autor: Philipp Hägerich 
 List die passenden texturen ein und speichert sie in einen Struct*/
 
-void
+#include<stdlib.h>
+
+
+void einlesenTexturen(struct *tex_rest[], char pfad[], int index)
+{
+	FILE *in;
+	char temp[20];
+	int i,j;
+	
+	in = fopen(pfad,"r");
+
+	for(i=0;i<21;i++)
+	{
+		fgets(temp,19,in);
+		
+		for(j=0;j<18;j++)
+		{
+			(*tex_rest)[index].textur[i][j] = temp [j];
+		}
+	}
+}
